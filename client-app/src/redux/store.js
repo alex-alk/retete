b87a39ad5f79f5e5cd349cc842a48a3d1013385c
Alex <alexandruleonte06@yahoo.com>
2022-07-06 16:19:53 +0300

    changes

--- client-app/src/redux/store.js
+++ client-app/src/redux/store.js
@@ -3,7 +3,7 @@ import authReducer from "./authReducer";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-const configureStore = (addLogger = true) => {
+const configureStore = (addLogger = false) => {
   const middleware = addLogger
     ? applyMiddleware(thunk, logger)
     : applyMiddleware(thunk);
